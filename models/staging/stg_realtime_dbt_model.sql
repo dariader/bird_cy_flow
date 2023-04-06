06956cca5ac3e18911b8ed25442732c79d0779ac
dariader <dary.spb@gmail.com>
2023-04-06 19:50:06 +0300

    fixes for dbt config

--- models/staging/stg_realtime_dbt_model.sql
+++ models/staging/stg_realtime_dbt_model.sql
@@ -3,5 +3,5 @@
 
 with realtime_source as (
     -- parse sci name into genus and family
-    select comName as common_name, sciName as scientific_name, lat, lng, custom_primary_key from {{source("bird_data_test", "realtime_data")}}
+    select comName as common_name, sciName as scientific_name, lat, lng, cast(custom_primary_key as string) as custom_primary_key from {{source("bird_data_test", "realtime_data")}}
 ) select * from realtime_source limit 151
