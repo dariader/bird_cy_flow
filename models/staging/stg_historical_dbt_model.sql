07f5df21e85543c140a86e028d07a9d1afd2b77f
dariader <dary.spb@gmail.com>
2023-04-07 00:26:39 +0300

    added link to the report

--- models/staging/stg_historical_dbt_model.sql
+++ models/staging/stg_historical_dbt_model.sql
@@ -2,7 +2,15 @@
 
 with historical_source as (
     -- add common name
-    select family, genus, species as scientific_name, decimalLatitude as lat, decimalLongitude as lng, catalogNumber as custom_primary_key, year, month, row_number() over(partition by genus, year) as rn
+    select family,
+     genus,
+      species as scientific_name,
+       decimalLatitude as lat,
+        decimalLongitude as lng,
+         catalogNumber as custom_primary_key,
+         cast(year, timestamp) as year,
+         cast(month, timestamp) as month,
+     row_number() over(partition by genus, year) as rn
      from {{source("bird_data_test", "historical_data")}}
      where taxonRank='SPECIES')
 select * from historical_source
