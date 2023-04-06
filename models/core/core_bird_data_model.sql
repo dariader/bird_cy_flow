7cee52bc46c11b1f3e22a003740f49bcb02baeb0
dariader <dary.spb@gmail.com>
2023-04-06 18:42:03 +0300

    fixes for dbt config

--- models/core/core_bird_data_model.sql
+++ models/core/core_bird_data_model.sql
@@ -7,7 +7,7 @@ with realtime_data as (
 historical_data as (
     select *,
     from {{ ref('stg_historical_dbt_model') }}
-),
+)
 select * from realtime_data,
 full outer join historical_data
 on historical_data.custom_primary_key = realtime_data.custom_primary_key;
