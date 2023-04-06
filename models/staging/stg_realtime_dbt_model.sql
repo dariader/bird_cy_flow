4fae79c4ca61ad47159f7d1667b0edf474800000
dariader <dary.spb@gmail.com>
2023-04-07 00:06:54 +0300

    added link to the report

--- models/staging/stg_realtime_dbt_model.sql
+++ models/staging/stg_realtime_dbt_model.sql
@@ -3,5 +3,14 @@
 
 with realtime_source as (
     -- parse sci name into genus and family
-    select comName as common_name, sciName as scientific_name, lat, lng, cast(custom_primary_key as string) as custom_primary_key from {{source("bird_data_test", "realtime_data")}}
+    select
+    comName as common_name,
+     sciName as scientific_name,
+      lat,
+       lng,
+        cast(custom_primary_key as string) as custom_primary_key
+        obsDt as observation_date,
+        date_trunc(observation_date, year) as year,
+        date_trunc(observation_date, year) as month,
+        from {{source("bird_data_test", "realtime_data")}}
 ) select * from realtime_source limit 151
