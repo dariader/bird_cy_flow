715d9fc9eb4a10dbbbfb67b36f382d9c45c50bc8
dariader <dary.spb@gmail.com>
2023-04-09 19:40:35 +0300

    fixes for dbt config

--- models/staging/stg_realtime_dbt_model.sql
+++ models/staging/stg_realtime_dbt_model.sql
@@ -10,7 +10,7 @@ with realtime_source as (
        lng,
         cast(custom_primary_key as string) as custom_primary_key,
         cast(obsDt as timestamp) as observation_date,
-        date_trunc(cast(obsDt as date), year) as year,
-        date_trunc(cast(obsDt as date), month) as month,
+        DATETIME_TRUNC(obsDt, year) as year,
+        DATETIME_TRUNC(obsDt, month) as month,
         from {{source("bird_data_test", "realtime_data")}}
 ) select * from realtime_source limit 151
